ad12149cdc3393fac74bd95b416b796fa9c921ea
kodi <dharmikpatel7112@gmail.com>
2023-02-21 17:26:19 +0530

    print response from mailchimp

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.post("/",function (req,res) {
   };
   const request = https.request(url,options,function (response) {
     response.on('data',function (data) {
-      console.log(JSON.parse(data).errors);
+      console.log(JSON.parse(data));
     })
     if(response.statusCode === 200) res.sendFile(__dirname + "/success.html");
     else res.sendFile(__dirname+"/failure.html");
