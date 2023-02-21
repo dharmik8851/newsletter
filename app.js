8c0dc58d751404f4630714cb65799082b19654ac
kodi <dharmikpatel7112@gmail.com>
2023-02-21 17:11:20 +0530

    error.length solve

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ app.post("/",function (req,res) {
   var error;
   const request = https.request(url,options,function (response) {
     response.on('data',function (data) {
-      error = JSON.parse(data).errors.length;
-      if(error!=0) console.log(JSON.parse(data).errors);
+      //error = JSON.parse(data).errors.length;
+      //if(error!=0) console.log(JSON.parse(data).errors);
     })
     if(response.statusCode === 200) res.sendFile(__dirname + "/success.html");
     else res.sendFile(__dirname+"/failure.html");
